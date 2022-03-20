0e42164a7e33c34a41a4671d069dbed3324d0718
Cintia Pedroso <idd_mpedroso@uolinc.com>
2022-03-20 16:50:09 -0300

    DMD-001 - Create service layer

---
+++ src/main/java/br/com/diamond/investhelper/service/impl/StockServiceImpl.java
@@ -0,0 +1,9 @@
+package br.com.diamond.investhelper.service.impl;
+
+import br.com.diamond.investhelper.service.StockService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockServiceImpl implements StockService {
+}
