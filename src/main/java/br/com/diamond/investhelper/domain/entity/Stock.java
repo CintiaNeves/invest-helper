bd4ffb80e9457a96b49a8f91d0ea1bf62f972b41
Cintia Pedroso <idd_mpedroso@uolinc.com>
2022-03-20 16:55:24 -0300

    DMD-001 - Add @Entity annotation in Stock class

--- src/main/java/br/com/diamond/investhelper/domain/entity/Stock.java
+++ src/main/java/br/com/diamond/investhelper/domain/entity/Stock.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -16,6 +17,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 @Data
 @Table
+@Entity
 public class Stock {
 
   @Id
