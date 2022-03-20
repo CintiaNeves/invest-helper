1656bd6293a45e66dabd1119b7ea5d2757f18398
Cintia Pedroso <idd_mpedroso@uolinc.com>
2022-03-20 15:51:03 -0300

    Inital commit

---
+++ src/main/java/br/com/diamond/investhelper/InvestHelperApplication.java
@@ -0,0 +1,13 @@
+package br.com.diamond.investhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diamond/investhelper/InvestHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diamond.investhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
