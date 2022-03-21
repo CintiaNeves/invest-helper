0c2ffb77d3388e80eff51dad8a2dfca3d78e49f7
Cintia Pedroso <idd_mpedroso@uolinc.com>
2022-03-21 18:54:56 -0300

    DMD-001 - Ignore mapping of unused fields

--- src/main/java/br/com/diamond/investhelper/mapper/StockMapper.java
+++ src/main/java/br/com/diamond/investhelper/mapper/StockMapper.java
@@ -5,10 +5,18 @@ import br.com.diamond.investhelper.dto.request.StockRequest;
 import br.com.diamond.investhelper.dto.response.StockResponse;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface StockMapper {
 
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "name", ignore = true)
+  @Mapping(target = "stockType", ignore = true)
+  @Mapping(target = "department", ignore = true)
+  @Mapping(target = "price", ignore = true)
+  @Mapping(target = "datCreate", ignore = true)
+  @Mapping(target = "datUpdate", ignore = true)
   Stock stockFromRequest(StockRequest stockRequest);
 
   StockResponse stockToStockResponse(Stock stock);
